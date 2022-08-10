a19cddfb47e7d9ceedb5c838a5d5d1809ef2526d
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-09 20:04:14 -0400

    bc i can't read

--- queries.sql
+++ queries.sql
@@ -1,1 +1,20 @@
 -- write your queries here
+SELECT * FROM owners o 
+   OUTER JOIN vehicles v 
+   ON o.id-v.owner_id;
+
+SELECT first_name, last_name, COUNT(owner_id) 
+   FROM owners o 
+   JOIN vehicles v 
+   ON o.id=v.owner_id 
+   GROUP BY (first_name, last_name) 
+   ORDER BY first_name;
+
+SELECT first_name, last_name, ROUND(AVG(price)) AS avg, COUNT(owner_id) 
+   FROM owners o 
+   JOIN vehicles v 
+   ON o.id=v.owner_id 
+   GROUP BY (first_name, last_name) 
+   HAVING COUNT (owner_id) > 1 AND ROUND(AVG(price)) > 10000 
+   ORDER BY first_name 
+   DESC;
